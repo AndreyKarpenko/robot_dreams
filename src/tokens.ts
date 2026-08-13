d5cfe1389015b78ac9213e330adf21a45dd762fa
andreykarpenko <andreykarpenkoua@gmail.com>
2026-08-13 16:15:46 +0300

    hw-06 - fixed symbols

--- src/tokens.ts
+++ src/tokens.ts
@@ -4,10 +4,10 @@ export type Token<T> = symbol & {
     readonly __type?: T
 }
 
-export const APP_SERVICE = Symbol('APP_SERVICE') as Token<AppService>
-export const PRISMA_SERVICE = Symbol('PRISMA_SERVICE') as Token<PrismaService>
-export const AUTH_SERVICE = Symbol('AUTH_SERVICE') as Token<AuthService>
-export const USER_SERVICE = Symbol('USER_SERVICE') as Token<UserService>
+export const APP_SERVICE = Symbol.for('APP_SERVICE') as Token<AppService>
+export const PRISMA_SERVICE = Symbol.for('PRISMA_SERVICE') as Token<PrismaService>
+export const AUTH_SERVICE = Symbol.for('AUTH_SERVICE') as Token<AuthService>
+export const USER_SERVICE = Symbol.for('USER_SERVICE') as Token<UserService>
 
-export const INJECTABLE_METADATA = Symbol('INJECTABLE_METADATA')
-export const INJECT_METADATA = Symbol('INJECT_METADATA')
+export const INJECTABLE_METADATA = Symbol.for('INJECTABLE_METADATA')
+export const INJECT_METADATA = Symbol.for('INJECT_METADATA')
